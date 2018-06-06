314f09843def5893cdb5780cbcea6b03eee13996
Jiah Kwon <845872625@bun2-206-11.local>
2018-06-06 12:03:20 +1200

    wrote vehicle data

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+var vehicles = {
+  motorbike :{
+    person : 1 ,
+    day :[1, 2, 3, 4, 5],
+    price : 109,
+    distance : 100,
+    fuel : 3.7
+
+  },
+  smallCard : {
+    person : [1,2],
+    day : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    price : 129,
+    distance : 100,
+    fuel : 8.5
+
+  },
+  largeCar : {
+    person : [1, 2, 3, 4, 5],
+    day : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    price : 144,
+    distance : 100,
+    fuel : 9.7
+  },
+
+  motorHome : {
+    person : [2, 3, 4, 5, 6],
+    day : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16],
+    price : 200,
+    distance : 100,
+    fuel : 17
+  }
+}
