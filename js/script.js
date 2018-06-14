f35884e7e72959d98ea3f26e2cbb7438f994ca9d
Jiah Kwon <845872625@bun2-206-11.local>
2018-06-15 11:51:40 +1200

    button logic thinking needed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-//Data
 var vehicles = {
   motorbike :{
     person : 1 ,
@@ -36,26 +34,26 @@ var vehicles = {
 //
 // // SETTING UP MAP ---------------------------------------------------------------
 //
-// mapboxgl.accessToken = 'pk.eyJ1IjoiamlhaGt3b24iLCJhIjoiY2ppOWR0b3Q2MHZoeTNwcGFwdjRqcGx2YyJ9.OCQ5S6NvpQir0tyfiUD3vQ';
-//
-// // works within NZ
-// // var bounds = [
-// //   [-34.23025627,172.43635083],
-// //   [-47.29064213,167.27277661]
-// // ]
-// var map = new mapboxgl.Map({
-// container: 'map',
-// style: 'mapbox://styles/jiahkwon/cji9fzmxi2hdd2smlfizy96tl'
-// // maxBounds: bounds
-//
-// });
-//
-//
-// // Add MapboxDirections
-// map.addControl(new MapboxDirections({
-//     accessToken: mapboxgl.accessToken
-// }), 'top-right');
-// //miles to km
+mapboxgl.accessToken = 'pk.eyJ1IjoiamlhaGt3b24iLCJhIjoiY2ppOWR0b3Q2MHZoeTNwcGFwdjRqcGx2YyJ9.OCQ5S6NvpQir0tyfiUD3vQ';
+
+// works within NZ
+// var bounds = [
+//   [-34.23025627,172.43635083],
+//   [-47.29064213,167.27277661]
+// ]
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/jiahkwon/cji9fzmxi2hdd2smlfizy96tl'
+// maxBounds: bounds
+
+});
+
+
+// Add MapboxDirections
+map.addControl(new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+}), 'top-right');
+//miles to km
 
 //-------------------date picker--------
 
@@ -86,15 +84,16 @@ $( function() {
 
 
 var nextArrow = document.querySelector('.fa-angle-right');
+var nextArrowArray = [];
 var previousArrow = document.querySelector('.fa-angle-left');
+var previousArrowArray = [];
 var formpages = document.querySelectorAll('.page');
 var directionsBtn = document.querySelectorAll('.pageDirection');
 var formArray = [].slice.call(formpages);
-var nextArrowArray = [];
 var currentPage = 0 ; // currentpage shows
 
-//storing form page in to Array
 
+//storing form page in to Array
   formArray.push(formpages);
 
  // show just one div
@@ -111,21 +110,41 @@ CurrentPageShow()
 
 // when next arrow is clicked. show next pages.
 nextArrow.addEventListener('click' , nextPageArrow , false);
+previousArrow.addEventListener('click' , backArrow , false);
 
+// when right arrow is clicked, move to next page
 function nextPageArrow() {
   console.log(formArray);
     nextArrowArray.push(nextArrow);
     console.log(nextArrowArray);
-    for (var i = 0; i < nextArrowArray.length; i++) {
-      if ((formArray.length == 1 ) < (nextArrowArray.length == 1 )) {
-        console.log('1');
+    //hide and show divs
+      previousArrow.style.display = 'block';
+      if ((formArray.length == 1 ) < (nextArrowArray.length <= 1 )) {
         formArray[0].style.display = 'none';
         formArray[1].style.display = 'block';
         formArray[2].style.display = 'none';
-      } else if ((formArray.length == 2 ) < (nextArrowArray.length == 2 )) {
+
+      } else if ((formArray.length == 2 ) < (nextArrowArray.length <=2 )) {
         formArray[0].style.display = 'none';
         formArray[1].style.display = 'none';
         formArray[2].style.display = 'block';
       }
+
+}
+
+
+function backArrow () {
+  previousArrowArray.push(previousArrow);
+  console.log(previousArrowArray);
+  for (var i = 0; i < previousArrowArray.length; i++) {
+    if ((formArray[0] ) && (previousArrowArray[0]) ) {
+      formArray[0].style.display = 'block';
+      formArray[1].style.display = 'none';
+      formArray[2].style.display = 'none';
+    } else if ((formArray.length == 2) < (previousArrowArray.length == 2)) {
+      formArray[0].style.display = 'none';
+      formArray[1].style.display = 'block';
+      formArray[2].style.display = 'none';
     }
+  }
 }
