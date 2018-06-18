ede8fd04f897b08c069781c09302d5bf0f7ba0f6
Jiah Kwon <845872625@bun2-206-11.local>
2018-06-19 11:46:38 +1200

    validation and form steps form W3

--- js/script.js
+++ js/script.js
@@ -124,44 +124,106 @@ function validateForm(){
 
 
 
-// when next arrow is clicked. show next pages.
-nextArrow.addEventListener('click' , nextPageArrow , false);
-previousArrow.addEventListener('click' , backArrow , false);
 
-// when right arrow is clicked, move to next page
-function nextPageArrow() {
-  console.log(formArray);
-    nextArrowArray.push(nextArrow);
-    console.log(nextArrowArray);
-    //hide and show divs
-      previousArrow.style.display = 'block';
-      if ((formArray.length == 1 ) < (nextArrowArray.length == 1 )) {
-        formArray[0].style.display = 'none';
-        formArray[1].style.display = 'block';
-        formArray[2].style.display = 'none';
+//---------------------------- form validation and page steps form w3 school
 
-      } else if ((formArray.length == 2 ) < (nextArrowArray.length ==2 )) {
-        formArray[0].style.display = 'none';
-        formArray[1].style.display = 'none';
-        formArray[2].style.display = 'block';
-      }
- validateForm();
-}
+var x = document.getElementsByClassName('page');
+var currentTab = 0; // Current tab is set to be the first tab (0)
+
+  showTab(currentTab); // Display the crurrent tab
+
+  function showTab(n) {
+  // This function will display the specified tab of the form...
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.querySelector('.fa-angle-left').style.display = "none";
+  } else {
+    document.querySelector('.fa-angle-left').style.display = "block";
+  }
+  if (n == (x.length - 1)) {
+    document.querySelector('.fa-angle-right').style.display = "inline";
+  }
+
+  }
 
 
-function backArrow () {
-  previousArrowArray.push(previousArrow);
-  console.log(previousArrowArray);
-    if ((formArray[0] ) && (previousArrowArray[0]) ) {
-      formArray[0].style.display = 'block';
-      formArray[1].style.display = 'none';
-      formArray[2].style.display = 'none';
-    } else if ((formArray.length == 2) < (previousArrowArray.length == 2)) {
-      formArray[0].style.display = 'none';
-      formArray[1].style.display = 'block';
-      formArray[2].style.display = 'none';
+  function nextPrev(n) {
+  // This function will figure out which tab to display
+
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.querySelector('.needs-validation').submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+  }
+
+  function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  var x = document.getElementsByClassName('page');
+  y = x[currentTab].getElementsByTagName('input');
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == '') {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
     }
-}
+  }
+  return valid; // return the valid status
+  }
+
+// form steps form me. logical thingking needed
+// // when next arrow is clicked. show next pages.
+// nextArrow.addEventListener('click' , nextPageArrow , false);
+// previousArrow.addEventListener('click' , backArrow , false);
+//
+// // when right arrow is clicked, move to next page
+// function nextPageArrow() {
+//   console.log(formArray);
+//     nextArrowArray.push(nextArrow);
+//     console.log(nextArrowArray);
+//     //hide and show divs
+//       previousArrow.style.display = 'block';
+//       if ((formArray.length == 1 ) < (nextArrowArray.length == 1 )) {
+//         formArray[0].style.display = 'none';
+//         formArray[1].style.display = 'block';
+//         formArray[2].style.display = 'none';
+//
+//       } else if ((formArray.length == 2 ) < (nextArrowArray.length ==2 )) {
+//         formArray[0].style.display = 'none';
+//         formArray[1].style.display = 'none';
+//         formArray[2].style.display = 'block';
+//       }
+//  validateForm();
+// }
+//
+//
+// function backArrow () {
+//   previousArrowArray.push(previousArrow);
+//   console.log(previousArrowArray);
+//     if ((formArray[0] ) && (previousArrowArray[0]) ) {
+//       formArray[0].style.display = 'block';
+//       formArray[1].style.display = 'none';
+//       formArray[2].style.display = 'none';
+//     } else if ((formArray.length == 2) < (previousArrowArray.length == 2)) {
+//       formArray[0].style.display = 'none';
+//       formArray[1].style.display = 'block';
+//       formArray[2].style.display = 'none';
+//     }
+// }
 
 
 //------------------------ input button
@@ -187,3 +249,16 @@ $('[data-quantity="minus"]').click(function(e){
         $('#inputField').val(currentValue = 1);
     }
 });
+
+
+
+// buttons checked dynamic
+var cars = document.getElementsByClassName('car');
+var carsArray = [];
+console.log(cars);
+
+function checked (){
+  carsArray.push(cars);
+  console.log(carsArray[1].value);
+}
+checked();
