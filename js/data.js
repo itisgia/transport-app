2f72bce7300352a8524baac8fe83d273dfcbf1a5
Jiah Kwon <845872625@bun2-206-11.local>
2018-06-22 11:09:34 +1200

    miles to km and add first instruction page

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 var vehicles = {
   motorbike :{
+    name: 'Yamaha YZF-R1',
     person : 1 ,
     day :[1, 2, 3, 4, 5],
     price : 109,
@@ -8,6 +9,7 @@ var vehicles = {
 
   },
   smallCard : {
+    name : 'Kia Picanto Si',
     person : [1,2],
     day : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     price : 129,
@@ -16,6 +18,7 @@ var vehicles = {
 
   },
   largeCar : {
+    name : 'Range Rover SV Coupe',
     person : [1, 2, 3, 4, 5],
     day : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     price : 144,
@@ -24,6 +27,7 @@ var vehicles = {
   },
 
   motorHome : {
+    
     person : [2, 3, 4, 5, 6],
     day : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 , 16],
     price : 200,
--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
 
+
+//variables
+
+var day = 0;
+var distance = 0;
 // // SETTING UP MAP ---------------------------------------------------------------
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiamlhaGt3b24iLCJhIjoiY2ppOWR0b3Q2MHZoeTNwcGFwdjRqcGx2YyJ9.OCQ5S6NvpQir0tyfiUD3vQ';
 
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/jiahkwon/cji9fzmxi2hdd2smlfizy96tl'
-// maxBounds: bounds
-
+    container: 'map',
+    style: 'mapbox://styles/jiahkwon/cji9fzmxi2hdd2smlfizy96tl'
+    // maxBounds: bounds
 });
 
 
-// Add MapboxDirections
 map.addControl(new MapboxDirections({
-    accessToken: mapboxgl.accessToken
+    accessToken: mapboxgl.accessToken,
+    unit: 'metric'//miles to km
 }), 'top-right');
-//miles to km
+
+
+
+// set two directions
+ // $('fa-angle-right').prop('disabled', true)
+
 
 //-------------------date picker && date calculate----------------
 
 
 //code refactoring
 $( function() {
-
   var select=function(dateStr) {
       var resultText = document.querySelector('.datechoosen');
       var d1 = $('#datepicker').datepicker('getDate');
@@ -30,24 +38,19 @@ $( function() {
       var timeDiff = d2 - d1;
       var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       console.log(daysDiff);
-      resultText.innerText = 'You\'re traveling with ' + daysDiff + ' days'
+      resultText.innerText = 'You\'re traveling ' + daysDiff + ' days with us 🧡'
       if ( daysDiff > 15) {
         alert('you\'ve selected more than 15 days 👻')
       }
   }
     //start date
     $('#datepicker').datepicker({
-      onSelect: select
+        onSelect: select
     });
-
     //end Date
-    $('#datepickerB').datepicker({onSelect: select});
+    $('#datepickerB').datepicker({
+        onSelect: select});
 });
-
-
-  //calculate how long they travel in NZ
-
-
 
 // --------form page function------------------------------------------------------------------------------------------
 
@@ -65,61 +68,55 @@ var currentPage = 0 ; // currentpage shows
 
 var x = document.getElementsByClassName('page');
 var currentTab = 0; // Current tab is set to be the first tab (0)
-
   showTab(currentTab); // Display the crurrent tab
-
   function showTab(n) {
   // This function will display the specified tab of the form...
-  x[n].style.display = "block";
+      x[n].style.display = "block";
   //... and fix the Previous/Next buttons:
-    if (n == 0) {
-      document.querySelector('.fa-angle-left').style.display = "none";
-    } else {
-      document.querySelector('.fa-angle-left').style.display = "block";
+      if (n == 0) {
+          document.querySelector('.fa-angle-left').style.display = "none";
+      } else {
+          document.querySelector('.fa-angle-left').style.display = "block";
+      }
+      if (n == (x.length - 1)) {
+        document.querySelector('.fa-angle-right').style.display = "none";
+      }
     }
-    if (n == (x.length - 1)) {
-    document.querySelector('.fa-angle-right').style.display = "none";
-
-    }
-
-  }
-
 
   function nextPrev(n) {
   // This function will figure out which tab to display
-
   // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
-  // Hide the current tab:
-  x[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
-  // if you have reached the end of the form...
-  if (currentTab >= x.length) {
-    // ... the form gets submitted:
-    document.querySelector('.needs-validation').submit();
-    return false;
+      if (n == 1 && !validateForm()) return false;
+      // Hide the current tab:
+      x[currentTab].style.display = "none";
+      // Increase or decrease the current tab by 1:
+      currentTab = currentTab + n;
+      // if you have reached the end of the form...
+      if (currentTab >= x.length) {
+        // ... the form gets submitted:
+        document.querySelector('.needs-validation').submit();
+        return false;
   }
   // Otherwise, display the correct tab:
-  showTab(currentTab);
+    showTab(currentTab);
   }
 
   function validateForm() {
   // This function deals with validation of the form fields
-  var x, y, i, valid = true;
-  var x = document.getElementsByClassName('page');
-  y = x[currentTab].getElementsByTagName('input');
+      var x, y, i, valid = true;
+      var x = document.getElementsByClassName('page');
+      y = x[currentTab].getElementsByTagName('input');
   // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == '') {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false
-      valid = false;
-    }
-  }
-  return valid; // return the valid status
+      for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == '') {
+          // add an "invalid" class to the field:
+          y[i].className += " invalid";
+          // and set the current valid status to false
+          valid = false;
+        }
+      }
+      return valid; // return the valid status
   }
 
 
@@ -129,12 +126,12 @@ var currentValue = parseInt(inputNum.value);
 
 // when plus is clicked, value increased
 $('[data-quantity="plus"]').click(function(e){
-  e.preventDefault(); // stop preventing it working as
-  if (inputNum.value < 6 ) {
-      $('#inputField').val(currentValue +=1); //keep add number when + is clicked
-  } else {
-      $('#inputField').val(currentValue = 6); //maximum 6
-  }
+    e.preventDefault(); // stop preventing it working as
+    if (inputNum.value < 6 ) {
+        $('#inputField').val(currentValue +=1); //keep add number when + is clicked
+    } else {
+        $('#inputField').val(currentValue = 6); //maximum 6
+    }
 });
 
 // when minus is clicked, value decreased
@@ -148,20 +145,16 @@ $('[data-quantity="minus"]').click(function(e){
 });
 
 
-
 // buttons checked dynamic
 var cars = document.getElementsByClassName('car');
 function checked (){
-  // carsArray.push(cars);
-  for(var i = 0; i < cars.length; i++){
-    //when car buttons are clicked. background changes
-    cars[i].addEventListener('click', function (){
-      console.dir(this);
-      this.classList.toggle('change'); // when car is clicked, background color toggle.
-      // this.classList.remove('change');
-      // this.classList.remove('change');
-    } ,false) //event lister
-  }//for loop ENDS
+    for(var i = 0; i < cars.length; i++){
+      //when car buttons are clicked. background changes
+      cars[i].addEventListener('click', function (){
+        console.dir(this);
+        this.classList.toggle('change'); // when car is clicked, background color toggle.
+      } ,false) //event lister
+    }//for loop ENDS
 }//function ENDS
 checked();
 
